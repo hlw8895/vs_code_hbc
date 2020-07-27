26380d85d740d2634dd96106a3a25265cde5f406
hlw8895 <60841139+hlw8895@users.noreply.github.com>
2020-07-27 21:10:24 +0800

    1

---
+++ .cpp/train/B.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main ()
+{
+    long long sum=0,A;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long sum = 0, A=0,max1=0;
+        int  n;
+        cin>>n;
+        for(int i=1;i<=n;i++)
+        {
+            cin>>A;
+            sum+=A;  
+            max1=max(max1,A);
+        }
+            if((sum-max1-max1)>=-1)
+            cout<<"YES";
+            else cout<<"NO";
+
+            cout<<endl;
+    }
+    return 0;
+}
