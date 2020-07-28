882722cbde2249a3c2506ddf8175f0eb199a1bd7
hlw8895 <60841139+hlw8895@users.noreply.github.com>
2020-07-28 21:14:02 +0800

    2

--- .cpp/train/F.cpp
+++ .cpp/train/F.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 int main()
 {
-    int n,num,sum,res;
+    int n,sum,res;
     cin >> n;
 
     res = num[0];
+    sum = num[0];
     for (int i = 0; i < n; i++)
     {
-
         sum = sum < 0 ? 0 : sum;
         res = max(res, sum + num[i]);
     }
