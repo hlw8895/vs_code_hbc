64d4287bad9ce6768f44fe998f700e3f211d4af2
hlw8895 <60841139+hlw8895@users.noreply.github.com>
2020-07-28 21:37:03 +0800

    \n

--- .cpp/train/F.cpp
+++ .cpp/train/F.cpp
@@ -1,19 +1,26 @@
 #include<iostream>
-#include<map>
 #include<algorithm>
+#include<array>
 using namespace std;
 int main()
 {
-    int n,sum,res;
-    cin >> n;
 
-    res = num[0];
-    sum = num[0];
+    int n;
+    array<int, 10000> nums{};
+    cin >> n;
+    for (int i = 0; i < n;i++)
+        cin >> nums[i];
+    int res = nums[0], sum = 0;
     for (int i = 0; i < n; i++)
     {
+        
         sum = sum < 0 ? 0 : sum;
-        res = max(res, sum + num[i]);
-    }
+        res = max(res, sum + nums[i]);
+        sum += nums[i];
+        }
+        res=res > 0 ? res : 0;
+        cout <<res << endl;
+
        
         return 0;
 }
